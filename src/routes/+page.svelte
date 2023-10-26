434cdd9f617f03141f0cc0557d48dd2e0a71eaf0
Mallory Hayr <hey@igalaxy.dev>
2023-10-25 22:24:32 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,20 @@
 	import { greet, add } from 'rust';
 
 	let name = '';
+	let a = 1;
+	let b = 1;
 </script>
 
 <input bind:value={name} placeholder="Enter your name" />
 <h1>{greet(name || 'SvelteKit')}</h1>
-<h2>1 + 1 = {add(1, 1)}</h2>
+<h2>
+	<input bind:value={a} placeholder="1" type="number" step="1" /> +
+	<input bind:value={b} placeholder="1" type="number" step="1" />
+	= {add(a, b)}
+</h2>
+
+<style>
+	input[type='number'] {
+		width: 40px;
+	}
+</style>
